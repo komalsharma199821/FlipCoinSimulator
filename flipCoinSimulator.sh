a66b03123e1117199e9494c3348973d2e9188975
Komal Sharma <komal.sharma@catseyesystems.com>
2020-09-27 20:15:53 +0530

    flip coin simulator use case 2 to repeat above ase multiple times in a loop and check who wins

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -10,3 +10,25 @@ then
 else
 	echo "Sorry,You Lost The Toss !";
 fi
+
+#2#
+#HEADS=1 #TAILS=0
+for i in {1..10}
+do
+	function generateRandomValue() {
+        case $1 in
+                1) generateRandomValue=0;;
+                *) generateRandomValue=1;;
+	esac
+        echo $generateRandomValue;
+}
+	generatedRandomValue=$( generateRandomValue $((RANDOM%2)) );
+	if [ $generatedRandomValue == 1 ]
+	then
+		timesHeadsAppeared=$((timesHeadsAppeared+1));
+	else
+		timesTailsAppeared=$((timesTailsAppeared+1));
+	fi
+done
+echo "Times Heads Won : "$timesHeadsAppeared;
+echo "Times Tails Won : "$timesTailsAppeared;
